b79ef77d27d0958e25d5d1b00c24fc6a7818f446
Mari Spinola <89092249+Mapinko@users.noreply.github.com>
2024-06-07 10:45:36 -0300

    feat: complete app

--- testeEco3/src/main/java/com/desafioinovacaoazul/testeeco3/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.desafioinovacaoazul.testeeco3.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
